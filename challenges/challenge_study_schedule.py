93c88f6476402e57a566b29d24811393a24f46b6
Thiago <thiago-470@outlook.com>
2023-06-01 17:33:27 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    
-    raise NotImplementedError
+
+    if target_time is None:
+        return None
+    count = 0
+    try:
+        for index in permanence_period:
+            initial_time, end_time = index
+            if initial_time <= target_time <= end_time:
+                count += 1
+        return count
+    except:
+        return None
+
