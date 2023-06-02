ce9a79a14ccd520bf692899032fe42305e18a244
Thiago <thiago-470@outlook.com>
2023-06-02 17:02:18 -0300

    req04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+def quick_sort(param):
+    if len(param) <= 1:
+        return param
+    if len(param) == 0:
+        return []
+
+    pivot_value = param[len(param) // 2]
+    smaller = [index for index in param if index < pivot_value]
+    equal = [index for index in param if index == pivot_value]
+    bigger = [index for index in param if index > pivot_value]
+
+    return quick_sort(smaller) + equal + quick_sort(bigger)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = ''.join(quick_sort(first_string.lower()))
+    second = ''.join(quick_sort(second_string.lower()))
+    if len(first) == 0 or len(second) == 0:
+        return (first, second, False)
+    boolean = first == second
+
+    return (first, second, boolean)
